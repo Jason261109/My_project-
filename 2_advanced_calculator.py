cf25a9388563ec6098295ff356a18c3fcafbfb0b
Jason261109 <j261109@gmail.com>
2024-03-13 10:52:06 +0800

    Add Minus

--- 2_advanced_calculator.py
+++ 2_advanced_calculator.py
@@ -1,13 +1,18 @@
-# Jason
+#
 # Output Test
+# Jason
+#
+
 # 1. Input
 x1 = input("Type x1:")
 x2 = input("Type x2:")
 op = input("Operator:")
+
 # 2. Process
 if op == '+':
     sum = int(x1) +int(x2)
 elif op == '-': 
     sum = int(x1) -int(x2)
+
 # 3. Output
 print(f"Result:{sum}")
